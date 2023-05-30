0000272167a9de6fbf8bc5905faae9a47337c721
Bekbol1 <kbekbol9@gmail.com>
2023-05-30 22:20:28 +0600

    2_6

--- app/src/main/java/com/example/bekbol_2_6/MainActivity.java
+++ app/src/main/java/com/example/bekbol_2_6/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.bekbol_2_6;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -17,23 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         TextView tv_entrance = findViewById(R.id.entry);
         TextView tv_welcome = findViewById(R.id.welcome);
         TextView tv_mini = findViewById(R.id.text1);
         Button button = findViewById(R.id.entry_button);
-        EditText et_login = findViewById(R.id.emailEdit);
+        EditText et_email = findViewById(R.id.emailEdit);
         EditText et_password = findViewById(R.id.passwordEdit);
 
-
-
         button.setOnClickListener(view -> {
-            String enteredLogin = et_login.getText().toString();
+            String enteredLogin = et_email.getText().toString();
             String enteredPassword = et_password.getText().toString();
 
             if(enteredLogin.equals("admin") && enteredPassword.equals("admin")) {
                 Toast.makeText(MainActivity.this,"Вы успешно вошли",Toast.LENGTH_SHORT).show();
-                et_login.setVisibility(View.GONE);
+                et_email.setVisibility(View.GONE);
                 et_password.setVisibility(View.GONE);
                 button.setVisibility(View.GONE);
                 tv_entrance.setVisibility(View.GONE);
@@ -43,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Неправильный логин или пароль", Toast.LENGTH_SHORT).show();
             }
         });
-        et_login.setOnFocusChangeListener((v, hasFocus) -> {
+        et_email.setOnFocusChangeListener((v, hasFocus) -> {
             if (hasFocus) {
                 getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
             }
