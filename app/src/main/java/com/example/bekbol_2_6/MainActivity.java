281677aa09119b6fa4e29bb12150070d67687054
Bekbol1 <kbekbol9@gmail.com>
2023-05-30 22:17:58 +0600

    2_6

--- app/src/main/java/com/example/bekbol_2_6/MainActivity.java
+++ app/src/main/java/com/example/bekbol_2_6/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.bekbol_2_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView tv_entrance = findViewById(R.id.entry);
+        TextView tv_welcome = findViewById(R.id.welcome);
+        TextView tv_mini = findViewById(R.id.text1);
+        Button button = findViewById(R.id.entry_button);
+        EditText et_login = findViewById(R.id.emailEdit);
+        EditText et_password = findViewById(R.id.passwordEdit);
+
+
+
+        button.setOnClickListener(view -> {
+            String enteredLogin = et_login.getText().toString();
+            String enteredPassword = et_password.getText().toString();
+
+            if(enteredLogin.equals("admin") && enteredPassword.equals("admin")) {
+                Toast.makeText(MainActivity.this,"Вы успешно вошли",Toast.LENGTH_SHORT).show();
+                et_login.setVisibility(View.GONE);
+                et_password.setVisibility(View.GONE);
+                button.setVisibility(View.GONE);
+                tv_entrance.setVisibility(View.GONE);
+                tv_mini.setVisibility(View.GONE);
+                tv_welcome.setVisibility(View.VISIBLE);
+            } else {
+                Toast.makeText(MainActivity.this, "Неправильный логин или пароль", Toast.LENGTH_SHORT).show();
+            }
+        });
+        et_login.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+            }
+        });
     }
 }
